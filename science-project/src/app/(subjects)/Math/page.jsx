43348f4ae11bb42416702a2fb60076871df43907
bayu-aji-nugroho <bayuastudies@gmail.com>
2025-08-03 01:36:45 +0700

    fix error (1)

--- science-project/src/app/(subjects)/Math/page.jsx
+++ science-project/src/app/(subjects)/Math/page.jsx
@@ -1,4 +1,5 @@
-import SubjectPage from "@/pages/subjectPage";
+import SubjectPage from "@/components/pages/subjectPage"
+
 
 export default function Page(){
     const data = {
@@ -6,12 +7,12 @@ export default function Page(){
         icon:"/Math.svg",
         infor:"Matematika adalah ilmu yang mempelajari pola, struktur, dan hubungan menggunakan logika dan simbol.",
         theme:["bg-lime-600","bg-lime-700"],
-        pin:{
+        pin:[{
             link:"/exam",
             prasyarat:[],
             judul:"Pretest Matematika",
             paragraft:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus deleniti incidunt temporibus asperiores consequatur sapiente commodi natus, neque accusantium eius quia voluptatibus delectus officiis voluptas, dignissimos doloremque dolor quae in!"
-            },
+            }],
 
     }
     return (
