01763f22285770d7b5a6be4c45d8377a32332903
bayu-aji-nugroho <bayuastudies@gmail.com>
2025-08-14 11:11:41 +0700

    change title

--- science-project/src/app/layout.jsx
+++ science-project/src/app/layout.jsx
@@ -5,6 +5,7 @@ import { Comic_Relief } from 'next/font/google';
 import { SpeedInsights } from "@vercel/speed-insights/next"
 import 'katex/dist/katex.min.css';
 import AuthContextProvider from "@/auth/authContext";
+import { icons } from "lucide-react";
 
 const fontC = Comic_Relief({
   subsets: ["latin"],
@@ -30,12 +31,21 @@ const geistMono = Geist_Mono({
 
 export const metadata = {
   title: "Science Project",
-  description: "Ayo eksplorasi dunia sains yang inovatif dan menarik secara gratis!",
+  description: "Eksplorasi dunia sains yang inovatif dan menarik secara gratis!",
+  icons: {
+    icon: "/icon.png"
+  },
+  openGraph: {
+    siteName: 'Science Project', 
+  },
 };
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning={true}>
+      <head>
+
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} ${fontC.variable} ${fontSub.variable}  antialiased bg-[#030712] font-main`}
       >
